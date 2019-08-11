f0d04a0709438d9e1f3854335b5c694149da532f
Silvia Kocsisova <silviakocsisovaCB5@MacBook-Air-3.kocsis-home.net>
2019-08-11 19:39:44 -0400

    building alphabeth is done

--- JavaScript/building.js
+++ JavaScript/building.js
@@ -10,12 +10,50 @@ for (let i=0;i<60;i++){
     let myDiv = document.createElement('div');
     myDiv.className="puzzleChar";
     myDiv.style.top=(puzzleStartTop+Math.floor(i/15)*(spaceBetweenRows+letterHeight)).toString()+'px';
-    myDiv.style.left= (puzzleStartLeft+Math.floor(i%15)*(spaceBetweenColumns+letterWidth)).toString()+'px';
+    myDiv.style.left= (puzzleStartLeft+(i%15)*(spaceBetweenColumns+letterWidth)).toString()+'px';
     myDiv.style.width=letterWidth.toString()+'px';
     myDiv.style.height=letterHeight.toString()+'px';
     let puzzleBox=document.getElementsByClassName("box2")[0]
-    console.log(puzzleBox)
+    //console.log(puzzleBox)
     puzzleBox.appendChild(myDiv)
+    // myDiv.addEventListener('click', goGreen);
+    // myDiv.addEventListener('mouseover', goGreen);
+}
+
+/////////////////  ALPHABET //////////////////////////////////////////////
+let alphaStartTop=570
+let alphaStartLeft=50
+let vowelMoveRight=500
+let alphaBetweenColumns=5
+let alphaSpaceBetweenRows=10
+let alphaWidth=32
+let alphaHeight=64
+/// VOWELS: aeiou
+let vovels="aeiou"
+for (let i=0;i<5;i++){
+    let myDiv = document.createElement('div');
+    myDiv.className="vowel";
+    myDiv.style.top= alphaStartTop.toString()+'px';
+    myDiv.style.left= (alphaStartLeft+i*(alphaBetweenColumns+alphaWidth)).toString()+'px';
+    myDiv.style.width=alphaWidth.toString()+'px';
+    myDiv.style.height=alphaHeight.toString()+'px';
+    let alphaBox=document.getElementsByClassName("box3A")[0]
+    //console.log(alphaBox)
+    alphaBox.appendChild(myDiv)
+    // myDiv.addEventListener('click', goGreen);
+    // myDiv.addEventListener('mouseover', goGreen);
+}
+/// CONSONANT
+for (let i=0;i<21;i++){
+    let myDiv = document.createElement('div');
+    myDiv.className="alpha";
+    myDiv.style.top= (alphaStartTop+alphaHeight+alphaSpaceBetweenRows).toString()+'px';
+    myDiv.style.left= (alphaStartLeft+i*(alphaBetweenColumns+alphaWidth)).toString()+'px';
+    myDiv.style.width=alphaWidth.toString()+'px';
+    myDiv.style.height=alphaHeight.toString()+'px';
+    let alphaBox=document.getElementsByClassName("box3A")[0]
+    //console.log(alphaBox)
+    alphaBox.appendChild(myDiv)
     // myDiv.addEventListener('click', goGreen);
     // myDiv.addEventListener('mouseover', goGreen);
 }
