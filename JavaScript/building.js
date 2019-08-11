c95c6c531414fdf53330145265b3edc7874970ab
Silvia Kocsisova <silviakocsisovaCB5@MacBook-Air-3.kocsis-home.net>
2019-08-11 18:49:55 -0400

    building puzzle done

--- JavaScript/building.js
+++ JavaScript/building.js
@@ -1,19 +1,18 @@
 
-let puzzleStartTop=0
-let puzzleStartRight=0
-let spaceBetweenColumns=5
+let puzzleStartTop=128
+let puzzleStartLeft=50
+let spaceBetweenColumns=15
 let spaceBetweenRows=5
-let letterWidth=32
+let letterWidth=50
 let letterHeight=64
 
-for (let i=0;i<10;i++){
+for (let i=0;i<60;i++){
     let myDiv = document.createElement('div');
     myDiv.className="puzzleChar";
-    //(puzzleStartTop+spaceBetweenColumns+i*letterWidth).toString()+'px';
-    myDiv.style.top=(puzzleStartTop+spaceBetweenRows+i*letterHeight).toString()+'px';
-    myDiv.style.right= (puzzleStartTop+spaceBetweenColumns+i*letterWidth).toString()+'px';
-    myDiv.style.width="32px";
-    myDiv.style.height="64px";
+    myDiv.style.top=(puzzleStartTop+Math.floor(i/15)*(spaceBetweenRows+letterHeight)).toString()+'px';
+    myDiv.style.left= (puzzleStartLeft+Math.floor(i%15)*(spaceBetweenColumns+letterWidth)).toString()+'px';
+    myDiv.style.width=letterWidth.toString()+'px';
+    myDiv.style.height=letterHeight.toString()+'px';
     let puzzleBox=document.getElementsByClassName("box2")[0]
     console.log(puzzleBox)
     puzzleBox.appendChild(myDiv)
