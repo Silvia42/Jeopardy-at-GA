1d9fcb7e345c569b4e1fb84c07419c67393d7aec
Silvia Kocsisova <silviakocsisovaCB5@MacBook-Air-3.kocsis-home.net>
2019-08-10 23:15:36 -0400

    wheel logo GA added

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,5 +1,14 @@
+// "slice" = circle sector
 let wheelAngle=0
-//let wheelCount=0
+let startAngle=20
+// beginnig Angle of wheel, first value in array
+// 24 slices: 360deg / 24 = 15 deg
+// midlle of slice +-7.5 deg
+let wheelValues=[-2,]
+// -2 = FREE PLAY
+// -1 = LOST A TURN
+//  0 = BANKRUPT
+// positive INT = $ value
 
 function spinWheel(angle){
     let target = document.querySelector('#spinWheel');
@@ -9,64 +18,38 @@ function spinWheel(angle){
       {transform: 'rotateZ('+(wheelAngle+360+angle).toString()+'deg)'}
     ], {duration : 512, iterations : 1,fill : "forwards",easing: 'ease-in-out'}
     );
- 
-
     player.addEventListener('finish', function() {
         //target.style.transform = 'translate(100px, 100px)';
         // wheelAngle=(wheelAngle+angle)%360;
         wheelAngle+=angle
         console.log(wheelAngle)
       });
-
 }
-
-
-
-
-// credits for this function: https://stackoverflow.com/questions/18481550/how-to-dynamically-create-keyframe-css-animations
-function insertCSSRule(ruleText)
-{
-    let sheets = document.styleSheets;
-    sheets.insertRule(ruleText);
-    // if(sheets.length == 0){
-    //     let style = document.createElement('style');
-    //     style.appendChild(document.createTextNode(""));
-    //     document.head.appendChild(style);
-    // }
-    // let sheet = sheets[sheets.length - 1];
-    // sheet.insertRule(ruleText, sheet.rules ? sheet.rules.length : sheet.cssRules.length);
-};
 
 $('body').ready(function() {
     console.log( "Body ready!" );  
-    spinWheel(180)
+    spinWheel(startAngle)
 });
-
-
 
 $('#spin').click(function() {
     console.log('something was click')
-    addAngle=Math.floor(Math.random()*360)
-    console.log('math',addAngle)
+    //addAngle=Math.floor(Math.random()*360)
+    // wheel will stop only in the middle of slice
+    // 15deg * 24 slices
+    addAngle=15*Math.floor(Math.random()*24)
+    //console.log('math',addAngle)
     wheelAngle+=addAngle+360
-
     spinWheel(addAngle)
-
     //console.log($("#spinWheel").css("animation"))
     //console.log($("#spinWheel").css("animation-iteration-count"))
     let ic=$("#spinWheel").css("animation-iteration-count");
     ic=(parseInt(ic)+1).toString()
-
     //console.log(typeof ic)
     //console.log(ic)
     $("#spinWheel").css("animation-iteration-count", ic);
     //$("#spinWheel").css("transform", "rotate(" + degrees + "deg)");
-
     // alert( "Handler for spin.click() called. \n"+ "animation-iteration-count = "+ic);
-
     "@keyframes keepRotate{0%{transform: rotateZ(0deg)}100% { transform: rotateZ(500deg)}}"
-    
-
   });
 
 
