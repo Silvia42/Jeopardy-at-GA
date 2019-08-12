ba3f1cd2a2242146adbfb2558a8ee96acdf523cf
Silvia Kocsisova <silviakocsisovaCB5@MacBook-Air-3.local>
2019-08-12 05:59:01 -0400

    gameState variable added

--- JavaScript/game.js
+++ JavaScript/game.js
@@ -1,3 +1,17 @@
+// GAME loop: spin wheel, Jepardy AQ, Pick a letter or solve a puzzle, REPEAT
+let gameState=0
+// 0 = game haven't start yet
+// -1 = game over
+// 1 = Is player 1 turn, spin a wheel
+// 2 = Is player 2 turn, spin a wheel
+// 3 = Is player 3 turn, spin a wheel
+// 11 = Is player 1 turn, Jepardy AQ
+// 12 = Is player 2 turn, Jepardy AQ
+// 13 = Is player 3 turn, Jepardy AQ
+// 101 = Is player 1 turn, Pick a letter or solve a puzzle
+// 102 = Is player 2 turn, Pick a letter or solve a puzzle
+// 103 = Is player 3 turn, Pick a letter or solve a puzzle
+
 
 let puzzleBox=document.getElementsByClassName("puzzleChar")
 let l=0
